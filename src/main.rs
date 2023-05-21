3e35c0c80c80c057ad238dc2dd78d80a488082c4
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-05-21 12:42:05 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Homepage"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build().mount("/", routes![index]).launch().await?;
+
+    Ok(())
+}
