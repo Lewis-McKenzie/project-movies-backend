4eefa545b506d467edf5b0e79262f5b5eae537c7
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-06-01 17:52:07 +0100

    added cors

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ extern crate rocket;
 
 use backend_practice::api::{get_all_routes, MovieService, ReviewService};
 use mongodb::bson::doc;
-use rocket::fairing::AdHoc;
+use rocket::{fairing::AdHoc, http::Method};
+use rocket_cors::{AllowedOrigins, CorsOptions};
 use std::error::Error;
 
 #[get("/")]
@@ -20,6 +21,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .attach(AdHoc::on_ignite("connecting...", |rocket| async {
             rocket.manage(ReviewService::new().await)
         }))
+        .attach(
+            CorsOptions::default()
+                .allowed_origins(AllowedOrigins::all())
+                .allowed_methods(
+                    vec![Method::Get, Method::Post, Method::Patch]
+                        .into_iter()
+                        .map(From::from)
+                        .collect(),
+                )
+                .allow_credentials(true)
+                .to_cors()?,
+        )
         .mount("/", routes![index])
         .mount("/api/v1", get_all_routes())
         .launch()
